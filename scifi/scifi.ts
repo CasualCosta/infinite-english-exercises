90b0fa9c8690ffba694cc0f9a764204802c5dec3
F. T. Costa <47283591+CasualCosta@users.noreply.github.com>
2022-10-19 15:18:53 -0300

    Update scifi.ts

--- scifi/scifi.ts
+++ scifi/scifi.ts
@@ -4,13 +4,45 @@
         "robots",
         "houses",
         "computers",
-        "cellphones",
+        "cell phones",
         "televisions",
         "tablets",
         "clothes",
         "animals",
-        "",
-        "",
-        "",
+        "drones"
     ]
+
+    let verbs: string[] = [
+        "fly",
+        "talk",
+        "think",
+        "teleport"
+    ]
+
+    class Question{
+        readonly description : string;
+        readonly answer : string;
+
+        constructor(subject: string, verb: string, questionType, answerType){
+            this.description = this.buildSentence(subject, verb, questionType);
+            this.answer = this.buildSentence(subject, verb, answerType);
+
+        }
+
+        buildSentence(subject: string, verb: string, type: number): string {
+            switch(type){
+                case -1: return `${subject}/${verb}`
+                case 0: return this.capitalizeFirstLetter(`${subject} will ${verb}.`);
+                case 1: return this.capitalizeFirstLetter(`${subject} won't ${verb}.`);
+                case 2: return this.capitalizeFirstLetter(`will ${subject} ${verb}?`);
+                case 3: return `Yes, they will.`;
+                case 4: return `No, they won't.`;
+            }
+            return "";
+        }
+
+        capitalizeFirstLetter(sentence: string){
+            return sentence.charAt(0).toUpperCase() + sentence.slice(1);
+        }
+    }
 }
